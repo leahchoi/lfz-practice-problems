146c800837bf405762b7ab09ef42c03babe578a3
leahchoi <soora913@gmail.com>
2019-01-05 15:37:12 -0800

    everything working except for makelabel

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,37 @@
 
-function sumArray(){
-	
+function sumArray(array) {
+    var result = 0;
+    for(let i = 0; i < array.length; i++){
+        result += array[i];
+    }
+    return result;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, num) {
+    var result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (num > array[i]) {
+            num -= array[i];
+            result.push(array[i]);
+        }
+    };
+    return result;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(array, num) {
+    var result = [];
+    for (let i = 0; i < array.length; i++){
+        if(array[i].length <= num){
+            result.push(array[i]);
+        }
+    }
+    return result;
 }
 
-function makeLabel(){
-
+function makeLabel(obj) {
+    
+   return (obj.greeting + obj.givenName + obj.familyName + '\n' + 
+    obj['home address'].streetNumber + obj['home address'].streetName + '\n' +
+    obj['home address'].city + obj['home address'].state + obj['home address'].zip)
+ 
 }
