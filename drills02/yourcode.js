aa51c38a59f901873bdd8129208840e5caac0fdd
leahchoi <soora913@gmail.com>
2019-01-07 22:30:05 -0800

    finished two practice problems

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,17 +1,48 @@
-
-
-function countOccurences(){
-
+function countOccurences(array, word){
+    var counter = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === word){
+            counter++;
+        }
+    }
+    return counter;
 }
 
-function wordLengths(){
-
+function wordLengths(array){
+    var result = [];
+    for(let i = 0; i < array.length; i++){
+        result.push(array[i].length);
+    }
+    return result;
 }
 
-function getMinMaxMean(){
-
+function getMinMaxMean(array){
+    var result = {
+        'min': array[0],
+        'max': array[0],
+        'mean': array[0]
+    };
+    for(let i = 1; i < array.length; i++){
+        if(array[i] < result.min){
+            result.min = array[i];
+        }
+        if(array[i] > result.max){
+            result.max = array[i]
+        }
+        result.mean += array[i];
+    }
+    result.mean = result.mean/array.length;
+    return result;
 }
 
-function findMode(){
-
+function findMode(array){
+    var numCounter = {};
+    for(let i = 0; i < array.length; i++){
+        if(numCounter[array[i]]){
+            numCounter[array[i]] += 1;
+        }else {
+            numCounter[array[i]] = 1;
+        }
+    }
+    return numCounter;
 }
