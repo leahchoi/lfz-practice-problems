717269477cfb62262f6d4aedd8734f56e3d18824
leahchoi <soora913@gmail.com>
2019-01-18 18:39:10 -0800

    working on drill 6

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,34 @@
 
 
-function removeClassFromElement(){
-	
+function removeClassFromElement(element, str) {
+    if (document.querySelector(element)) {
+        document.querySelector(element).classList.remove(str);
+    }
 }
 
-function toggleClassOnElement(){
-	
+function toggleClassOnElement(element, str) {
+    let elements = document.querySelectorAll(element);
+    if(element === '.' + str){
+        document.querySelector(element).classList.remove(str);
+    }else if (elements.length > 1) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.add(str);
+        }
+    }else {
+        document.querySelector(element).classList.add(str)
+    }
 }
 
-function hideElements(  ){
-	
+function hideElements() {
+
 }
 
-function addAttributeToElement( ){
-	
+function addAttributeToElement(element, attribute, str) {
+    document.querySelector(element).setAttribute(attribute, str);
 }
 
-function putPosInElement(  ){
-	
+function putPosInElement(str) {
+    return str;
 }
 
 
