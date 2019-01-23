180565c7158cffccbc45e8d3d49a360dedf9fd04
leahchoi <soora913@gmail.com>
2019-01-22 18:47:07 -0800

    finished practice 4

--- practice04/practice.js
+++ practice04/practice.js
@@ -1,3 +1,17 @@
-fuction sort(array){
-    
+function sort(array) {
+    let j = 0;
+    while (j < array.length) {
+        for (let i = 0; i < array.length - j - 1; i++) {
+            let temp;
+            if (array[i] > array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+        j++;
+    }
+    return array;
 }
+
+sort(['cat', 'dog', 'human', 'mouse']);
