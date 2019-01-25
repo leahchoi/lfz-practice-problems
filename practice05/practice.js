47231acda1ba209d66c2ae3433a92452317c0a84
leahchoi <soora913@gmail.com>
2019-01-24 16:23:16 -0800

    finished practice 5

---
+++ practice05/practice.js
@@ -0,0 +1,15 @@
+function fizzbuzz(){
+    let result = [];
+    for(let i = 1; i <= 100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            result.push('fizzbuzz');
+        } else if (i % 3 === 0){
+            result.push('fizz');
+        } else if (i % 5 ===0){
+            result.push('buzz')
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
+}
