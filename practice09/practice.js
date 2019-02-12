f97f02098d0a27c8ed5e8cad4e82a9fdc35e15fc
leahchoi <soora913@gmail.com>
2019-02-11 20:15:59 -0800

    working on practice 9

---
+++ practice09/practice.js
@@ -0,0 +1,14 @@
+// - `var myArray = ['cat','caterpillar','whale','accurate','smile','cactus','cute'];`
+//     - `fittingWords('cat', myArray);`
+//     - Output - `['cat','caterpillar','accurate','cactus']`
+
+function fittingWords(str, array){
+    let dictionary = {};
+    for(let i = 0; i < array.length; i++){
+        
+    }
+}
+
+// create a space to store all the letters counter for a word
+// iterate through every word in the array 
+// 
