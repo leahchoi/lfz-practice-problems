e8d020c3e479626de42f70d04aff629284b8ab02
leahchoi <soora913@gmail.com>
2019-02-17 12:53:50 +0900

    revised on practice 13

--- practice13/practice.js
+++ practice13/practice.js
@@ -3,12 +3,12 @@
 // And the resulting output:
 // -["2+5=7", "2-5=-3", "2*5=10", "2/5=0.4"];
 
-function math_sequences(num1, num2){
+function math_sequences(num1, num2) {
     let result = [];
     let op = ['+', '-', '/', '*'];
-    for(let i = 0; i < op.length; i++){
-        // since op is a string, it might not actually do the math for the result
-        result.push(`num1 ${op[i]} num2 = ${num1}${op[i]}${num2}`);
+    for (let i = 0; i < op.length; i++) {
+        let equation = num1 + op[i] + num2;
+        result.push(`num1 ${op[i]} num2 = ` + eval(equation))
     }
     return result;
 }
